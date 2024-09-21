69522feb4192aaa6235631d8d46b5212f6c34c79
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2024-09-21 23:08:55 +0300

    chore(build): add meson formatter

--- nix/treefmt/flake-module.nix
+++ nix/treefmt/flake-module.nix
@@ -3,20 +3,19 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem =
-    { ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs = {
-          nixfmt.enable = true;
-          yamlfmt.enable = true;
-          deadnix.enable = true;
-          just.enable = true;
-          mdformat.enable = true;
-          rustfmt.enable = true;
-          clang-format.enable = true;
-        };
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        nixfmt.enable = true;
+        yamlfmt.enable = true;
+        deadnix.enable = true;
+        just.enable = true;
+        mdformat.enable = true;
+        rustfmt.enable = true;
+        clang-format.enable = true;
+        meson.enable = true;
       };
     };
+  };
 }
